ef67f51538cf8438cf5bd6027d0d40c2de6170c7
Dejan Jarc <dj3778@student.uni-lj.si>
2023-11-12 11:44:30 +0100

    Squashed commit of the following:
    commit 2f7a8f202df24216bacddaf6a91c06f69c64c260
    Author: Jure-Z <82453768+Jure-Z@users.noreply.github.com>
    Date:   Sun Nov 12 11:29:30 2023 +0100
    
        added lib & tested render code on example scene
    
    commit 194fba2d680e49066dd4311789881c8e72611db5
    Author: Jure-Z <82453768+Jure-Z@users.noreply.github.com>
    Date:   Wed Nov 8 14:19:17 2023 +0100
    
        added engine to src
    
    commit 046b330d51e6aaacb99954b1f723dacabb94d817
    Author: Dejan Jarc <76999539+jarcdejan@users.noreply.github.com>
    Date:   Fri Nov 3 20:14:52 2023 +0100
    
        Update issue templates
    
        Predloge za issues. Zgolj optional.

---
+++ src/engine/core/MeshUtils.js
@@ -0,0 +1,43 @@
+import { quat, vec3, vec4, mat3, mat4 } from '../../../lib/gl-matrix-module.js';
+
+export function transformVertex(vertex, matrix,
+    normalMatrix = mat3.normalFromMat4(mat3.create(), matrix),
+    tangentMatrix = mat3.fromMat4(mat3.create(), matrix),
+) {
+    vec3.transformMat4(vertex.position, vertex.position, matrix);
+    vec3.transformMat3(vertex.normal, vertex.normal, normalMatrix);
+    vec3.transformMat3(vertex.tangent, vertex.tangent, tangentMatrix);
+}
+
+export function transformMesh(mesh, matrix,
+    normalMatrix = mat3.normalFromMat4(mat3.create(), matrix),
+    tangentMatrix = mat3.fromMat4(mat3.create(), matrix),
+) {
+    for (const vertex of mesh.vertices) {
+        transformVertex(vertex, matrix, normalMatrix, tangentMatrix);
+    }
+}
+
+export function calculateAxisAlignedBoundingBox(mesh) {
+    const initial = {
+        min: vec3.clone(mesh.vertices[0].position),
+        max: vec3.clone(mesh.vertices[0].position),
+    };
+
+    return {
+        min: mesh.vertices.reduce((a, b) => vec3.min(a, a, b.position), initial.min),
+        max: mesh.vertices.reduce((a, b) => vec3.max(a, a, b.position), initial.max),
+    };
+}
+
+export function mergeAxisAlignedBoundingBoxes(boxes) {
+    const initial = {
+        min: vec3.clone(boxes[0].min),
+        max: vec3.clone(boxes[0].max),
+    };
+
+    return {
+        min: boxes.reduce(({ min: amin }, { min: bmin }) => vec3.min(amin, amin, bmin), initial),
+        max: boxes.reduce(({ max: amax }, { max: bmax }) => vec3.max(amax, amax, bmax), initial),
+    };
+}
